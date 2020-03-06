1908b73c3d3e38fc65c2d7e1bee0a178ea84a8c5
ckarpo <48755658+ckarpo@users.noreply.github.com>
2020-03-06 17:32:19 +0000

    Update pageOne.cpp

--- pageOne.cpp
+++ pageOne.cpp
@@ -2,63 +2,73 @@
 
 //--------------------------------------------------------------
 void pageOne::setup(){
-    ofBackground(255);
-    ofSetBackgroundAuto(false);
+    //ofBackground(0);
+   // ofSetBackgroundAuto(false);
     save=0;
     nai=false;
-    //image.load("blah.png");
+    img.load("blah1.png");
+    baner1.load("Screen01_Title-24.png");
+   // baner2.load();
     numOfDrawing=0;
     lastNoD=0;
     
-   // for (int i=0; i<numOfDrawing; i++){
-    // image[numOfDrawing].load("blah"+ofToString(numOfDrawing)+".png");
-  //img.load("blah"+ofToString(numOfDrawing)+".png");
-   //}
-    
+
 }
 
 //--------------------------------------------------------------
 void pageOne::update(){
-//    if (numOfDrawing!=lastNoD){
-//        imgBuffer.push_front(image[numOfDrawing]);
-//    }
-   // for (int i=0; i<numOfDrawing; i++){
+
 }
 
 //--------------------------------------------------------------
-void pageOne::draw(){
-    int imgwidth= ofGetWidth()/5;
+void pageOne::draw(int i ){
     
     
+    if (i==1){
+                int imgwidth= ofGetWidth()/5;
+                //baner1.draw(0,0);
                 for (int i=0; i<numOfDrawing; i++){
                     ofPushMatrix();
-                    ofPushStyle();
+                    //ofPushStyle();
                     int imgWidth=image[i].getWidth();
                         ofSetColor(255);
                         ofScale(0.2,0.2);
-                        image[i].draw(i*imgWidth,0);
+                        image[i].draw(i*imgWidth,100);
                     
-                    ofPopStyle();
+                    //ofPopStyle();
                     ofPopMatrix();
                
                 }
-    
-  
+        
+
+    }
     //myDrawing[numOfDrawing].draw(mouseX,mouseY);
 }
+void pageOne::drawFullscreen(){
+ for (int i=0; i<numOfDrawing; i++){
+     ofSetColor(255);
+     image[i].draw(0,0);
+     
+ }
+
+    
+}
+
+
 //--------------------------------------------------------------
 void pageOne::takeScreenshot(int key){
     if (key==1){
         
         save=numOfDrawing;//axristo?
         numOfDrawing++;
-        cout<<numOfDrawing<<endl;
+        //cout<<numOfDrawing<<endl;
         
-        screenshot.grabScreen(0, 0, ofGetWidth(), ofGetHeight()-40);
+        screenshot.grabScreen(0, 0, ofGetWidth(), ofGetHeight()-190);
         image.push_back(screenshot);
         
         //  screenshot.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
-        //screenshot.save("blah"+ofToString(numOfDrawing)+".png");
+        screenshot.save("blah"+ofToString(numOfDrawing)+ofToString(ofGetTimestampString())+".png");
         
     }
 }
+
